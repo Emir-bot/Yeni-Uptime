5e2ad0aec239c90ea01977a13950138abbfdb993
Raxion <85894723+Emir-bot@users.noreply.github.com>
2026-02-15 00:54:25 +0300

    Update server.js

--- server.js
+++ server.js
@@ -36,7 +36,6 @@ db.set("linkler", [])
 })
 
 client.on("ready", () => {
-  //client.channels.cache.get("1017729963488718892").join()
   client.user.setActivity(`.help | ${db.get("linkler").length} aktif BOT!`)
   console.log(`Bot Başarılı Şekilde Atkif!`)
 })
